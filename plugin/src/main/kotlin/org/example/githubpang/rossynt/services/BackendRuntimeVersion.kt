7ad66bdbad1aa8db17556dc0df878ef27b597410
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2024-12-01 17:26:43 +0100

    Fix support for .NET 8

--- plugin/src/main/kotlin/org/example/githubpang/rossynt/services/BackendRuntimeVersion.kt
+++ plugin/src/main/kotlin/org/example/githubpang/rossynt/services/BackendRuntimeVersion.kt
@@ -3,4 +3,5 @@ package org.example.githubpang.rossynt.services
 internal enum class BackendRuntimeVersion(val majorVersion: Int, val directoryName: String) {
     DOT_NET_6(6, "net6.0"),
     DOT_NET_7(7, "net7.0"),
+    DOT_NET_8(8, "net8.0"),
 }
